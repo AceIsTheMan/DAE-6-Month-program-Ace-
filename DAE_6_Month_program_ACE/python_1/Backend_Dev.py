fb18d9ba5e042eec924d8bdc8b2270843c0cf36a
AceIsTheMan <acetheman0106@gmail.com>
2026-07-27 14:06:43 -0400

    web scraper start project

---
+++ DAE_6_Month_program_ACE/python_1/Backend_Dev.py
@@ -0,0 +1,28 @@
+# Testing functions
+
+username = input("Name?: ")
+
+# Gender selection
+
+while gender != "Male" or gender != "Female" or gender != "Other":
+    input(f"{username} Is Male. Female, or Other?: ")
+    gender = input("Male, Female, Other?: ")
+
+    if gender == "Male":
+        gender = "Mr"
+    elif gender == "Female":
+        gender = "Mrs"
+    elif gender == "Other":
+        gender = "The"
+#==================================
+# Gender Wall
+#==================================
+
+
+hometown = input("Where are you from?: ")
+
+def greet_user():
+    print ("=====================")
+    print (f"Hello {username} from {hometown}.")
+    print (f"How may I be of service ")
+    print ("=====================")
